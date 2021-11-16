259aa7cb11e29c3a17af5cfbd858104a875cb59c
Shaun Gill <shaung75@gmail.com>
2021-11-16 22:57:04 +0000

    Adding in 24 sensors

--- sykes-lane/sykes-lane.ino
+++ sykes-lane/sykes-lane.ino
@@ -4,13 +4,18 @@ CMRI cmri;
 
 // Define number of points
 #define numPoints 16
+#define sensors 24
 
 int pointPin[numPoints*2]; // Need twice number of pins as there are points (one for close, one for throw)
+int sensor[sensors]; // Allocating 24 sensors
 
 void setup() {
   Serial.begin(9600, SERIAL_8N2);
   Serial.println("CMRI Sykes Lane");
 
+  // Set up relay pins
+  // Pins 22-37 = Board 1
+  // Pins 46-61 = Board 2
   int Pin = 22; // Board 1 start pin
   
   for(int i=0; i<numPoints*2; i++) {
@@ -26,8 +31,19 @@ void setup() {
    
     pinMode(pointPin[i], OUTPUT);
     digitalWrite(pointPin[i], HIGH);
-
   }
+
+  // Set up sensor pins
+  int sensorPin = 14; // Sensors start pin
+  for (int i=0; i<sensors; i++) {
+    sensor[i] = sensorPin++;
+    
+    if((i+1)%8 == 0) {
+      sensorPin = sensorPin + 16;
+    }
+    pinMode(sensor[i], INPUT_PULLUP);
+  }
+
 }
 
 void loop() {
@@ -42,4 +58,8 @@ void loop() {
     }
   }
 
+  // Process sensors
+  for(int i = 0; i<sensors; i++) {
+    cmri.set_bit(i, !digitalRead(sensor[i]));
+  }
 }
